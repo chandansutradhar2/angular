e4bbc7a5c12a6b93eeef9b9c2ebfb8c731f736f9
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-09 15:52:55 +0530

    insitial website setup components

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarasoulComponent } from './carasoul/carasoul.component';
+import { CategoryComponent } from './category/category.component';
+import { MenubarComponent } from './menubar/menubar.component';
 
 @NgModule({
-  declarations: [AppComponent, DemoComponent],
+  declarations: [AppComponent, DemoComponent, HeaderComponent, FooterComponent, CarasoulComponent, CategoryComponent, MenubarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ first-app/src/app/carasoul/carasoul.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carasoul',
+  templateUrl: './carasoul.component.html',
+  styleUrls: ['./carasoul.component.css']
+})
+export class CarasoulComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
