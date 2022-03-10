7bf85fa5a2dab7d2c3f3ebb7bcfb79543b64d8bf
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-10 15:11:47 +0530

    clean code and naming convention

--- first-app/src/app/models/user.model.ts
+++ first-app/src/app/models/user.model.ts
@@ -1,11 +1,18 @@
 import { Address } from './address.model';
 
+//className and interface Name should start with Caps
+// all should strat
+
+function testMethod(num1: number, num2: number, num3?: number) {}
+
+testMethod(1, 5);
+
 interface User {
   id: string;
   firstName: string;
   lastName: string;
   email: string;
-  password?: string;
+  password?: string; //? to make optional
   mobileNo?: string;
   footSize?: number;
   gender?: GENDER;
@@ -14,9 +21,30 @@ interface User {
   dob?: number;
 }
 
+//variable/attribute/functionName/methodName all should be in camelCase
+
+//enum should be all caps
 export enum GENDER {
   'MALE' = 'MALE',
   'FEMALE' = 'FEMALE',
 }
 
 export default User;
+
+let addressObj: Address = {
+  addressType: '',
+  city: '',
+  country: '',
+  line1: '',
+  line2: '',
+  state: '',
+  zipCode: '',
+};
+
+let userObj: User = {
+  address: addressObj,
+  email: '',
+  firstName: '',
+  id: '',
+  lastName: '',
+};
