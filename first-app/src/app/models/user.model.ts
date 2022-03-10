0c8002c69607e6acc41f43242a4ed2291e127576
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-10 14:52:00 +0530

    enum demo

--- first-app/src/app/models/user.model.ts
+++ first-app/src/app/models/user.model.ts
@@ -8,8 +8,28 @@ export interface User {
   password?: string;
   mobileNo?: string;
   footSize?: number;
-  gender?: string;
+  gender?: GENDER;
   address?: Address;
   age?: number;
   dob?: number;
+}
+
+export enum GENDER {
+  'MALE' = 'MALE',
+  'FEMALE' = 'FEMALE',
+}
+
+let userObj: User = {
+  gender: GENDER.MALE,
+};
+
+let userObj2: User = {
+  gender: GENDER.MALE,
+};
+
+let userObj3: User = {
+  gender: GENDER.MALE,
+};
+
+if (userObj.gender == 'male') {
 }
