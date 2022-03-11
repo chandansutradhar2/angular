c3bea9df383e146cde9dc7d695e93360b5cfa78b
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-11 16:15:34 +0530

    sending request to server using httpClient

--- first-app/src/app/signin/signin.component.ts
+++ first-app/src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signin',
@@ -11,13 +12,29 @@ export class SigninComponent implements OnInit {
   password!: string;
 
   @Output() loginSuccess: EventEmitter<boolean> = new EventEmitter();
-  constructor() {}
+  //passing any class to contructor are handled by angular as dependecny
+  //angular will inject thoses depencdency at runtime (dependency injection)
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
   login() {
-    alert(this.email + ':' + this.password);
-    //user logged in succesfully
+    //todo: send request to the restapi : http://localhost:8000/user/authenticate
+    this.http
+      .post('http://localhost:8000/user/authenticate', {
+        Email: this.email,
+        Password: this.password,
+      })
+      .subscribe(
+        (res: any) => {
+          if (res.result) {
+            alert('token recieved:' + res.token);
+          }
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
     this.loginSuccess.emit(true);
   }
 
