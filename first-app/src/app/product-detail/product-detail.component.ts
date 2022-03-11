9a4ba3227351a4f384427c2c3050aca5f43901bc
chandan_naresh <chandansutradhar2@gmail.com>
2022-03-11 17:40:43 +0530

    added product service

--- first-app/src/app/product-detail/product-detail.component.ts
+++ first-app/src/app/product-detail/product-detail.component.ts
@@ -9,26 +9,7 @@ import { Product } from '../models/product.model';
 })
 export class ProductDetailComponent implements OnInit {
   products: Product[] = [];
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    let token = sessionStorage.getItem('token');
-    if (token) {
-      this.http
-        .get('http://localhost:8000/product/getAllProduct', {
-          headers: {
-            token: token,
-          },
-        })
-        .subscribe(
-          (r: any) => {
-            console.log('product recieved', r);
-            this.products = r as Product[];
-          },
-          (err) => console.log(err)
-        );
-    } else {
-      console.log('no token exists...');
-    }
-  }
+  ngOnInit(): void {}
 }
